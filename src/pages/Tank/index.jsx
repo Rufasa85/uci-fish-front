ed3edc3cdf861e8e86a9ee2479b5495efecdd8f4
Joe Rehfuss <jrehfuss@c02g81atq05q.domain>
2024-04-24 11:54:21 -0700

    adding stuff

--- src/pages/Tank/index.jsx
+++ src/pages/Tank/index.jsx
@@ -1,29 +1,39 @@
-//TODO: condintally render for no tank found
-import React, { useEffect, useState } from 'react'
-import "./style.css"
-import Fish from '../../components/Fish'
-import { useParams } from 'react-router-dom'
-import API from '../../utils/API'
+import React, { useEffect, useState } from "react";
+import "./style.css";
+import Fish from "../../components/Fish";
+import { useParams } from "react-router-dom";
+import API from "../../utils/API";
 const Tank = (props) => {
-  const {id} = useParams()
-  const [tank, setTank] = useState({})
+  const { id } = useParams();
+  const [tank, setTank] = useState({});
   useEffect(() => {
-   API.getOneTank(id).then(data=>{
-    setTank(data)
-   })
-  }, [])
-  
+    API.getOneTank(id).then((data) => {
+      setTank(data);
+    });
+  }, []);
+
   return (
     <>
-        <h2>{tank?.name}</h2>
-    <div className = "Tank">
-       {tank?.Fishes?.map(fsh=><Fish key={fsh.id} name={fsh.name} color={fsh.color} width={fsh.width}/>)}
-    </div>
-    <div className="seaFloor">
-
-    </div>
+      {!tank.name ? (
+        <h1>No such tank</h1>
+      ) : (
+        <>
+          <h2>{tank?.name}</h2>
+          <div className="Tank">
+            {tank?.Fishes?.map((fsh) => (
+              <Fish
+                key={fsh.id}
+                name={fsh.name}
+                color={fsh.color}
+                width={fsh.width}
+              />
+            ))}
+          </div>
+          <div className="seaFloor"></div>
+        </>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Tank
+export default Tank;
